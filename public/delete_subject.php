b3f4ce80fa5f4032061ad78ade2f6ec3c130d687
Alex Peng <jjpeng@ryerson.ca>
2017-05-29 14:21:05 -0400

    added subject create , delete and edit

---
+++ public/delete_subject.php
@@ -0,0 +1,24 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+
+<?php
+  $current_subject = find_subject_by_id($_GET["subject"]);
+  if(!$current_subject){
+    redirect_to("manage_content.php");
+  }
+
+  $id = $current_subject["id"];
+  $query = "DELETE FROM subjects WHERE id = {$id} LIMIT 1";
+  $result = mysqli_query($connection , $query);
+
+  if($result && mysqli_affected_rows($connection) == 1){
+    //Sucess
+    $_SESSION["message"] = "Subject deleted.";
+    redirect_to("manage_content.php");
+  } else {
+    // Failure
+    $_SESSION["message"] = "Subject deletion failed.";
+    redirect_to("manage_content.php?subject={$id}");
+  }
+?>
