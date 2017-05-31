8d1bbe98752f6ede265f67388fdd92074f0716d8
Alex Peng <jjpeng@ryerson.ca>
2017-05-30 23:31:08 -0400

    refractor admin page

--- public/admin.php
+++ public/admin.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<?php require_once("../includes/functions.php"); ?>
+<?php $layout_context = "admin"; ?>
+<?php include("../includes/layouts/header.php"); ?>
 
-<html lang="en">
-	<head>
-      <title>Maplestory Admin Page</title>
-	  <link href="stylesheets/public2.css" media="all" rel="stylesheet" type="text/css" />
-	</head>
-	<body>
-		<div id="header">
-			<h1>Maplestory</h1>
-		</div>
 
-		<div id="main"> 
+		<div id="main">
 			<div id="navigation">
 				<h2 >  &nbsp </h2>
 			</div>
-			<div id="page"> 
+			<div id="page">
 				<h2> Admin Menu</h2>
 				<p> Welcome to the admin area. <p>
-				<ul> 
+				<ul>
 					<li><a href ="manage_content.php"> Manage Website Content </a></li>
 					<li><a href ="manage_admin.php"> Manage Admin Users</a></li>
 					<li><a href ="logout.php"> Logout</a></li>
